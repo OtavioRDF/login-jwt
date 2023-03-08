4ba52bd2132a190e3e6a62e780a1966c5c936d72
OtavioRDF <otavio.rodf@gmail.com>
2023-03-08 15:23:36 -0300

    feat: swagger-autogen config

---
+++ src/swagger.ts
@@ -0,0 +1,29 @@
+import swaggerAutogen from 'swagger-autogen';
+import path from 'path';
+
+const API_PREFIX = 'api/v1'
+
+const doc = {
+    info: {
+      version: '1.0.0', // by default: '1.0.0'
+      title: 'Login-JWT', // by default: 'REST API'
+      description: 'Login-JWT login microservice, developed for a microservices study.',
+    },
+    host: '',
+    basePath: API_PREFIX, 
+    schemes: ['http'],
+    consumes: ['application/json'], 
+    produces: ['application/json'], 
+    tags: [ 
+      {
+        name: 'Login', 
+        description: '',
+      },
+    ],
+    securityDefinitions: {},
+};
+
+const outputFile = path.join(__dirname, '..', 'config/swagger-output.json');
+const endpointsFiles = [path.join(__dirname, './routes.ts')];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
